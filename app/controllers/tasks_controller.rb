4c54409bf2a604d9e13beb386bd96e285d7ec7eb
akitaka <syugen3619@gmail.com>
2021-08-04 14:01:35 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
 
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に作成されました'
